123bd70608210f54718175135605d6dad642f063
Sean Templeton <sean@templeton.io>
2017-10-25 20:40:04 -0500

    Show basic fixes for server file loading and include jQuery

--- projects/tic-tak-toe-game/server-side.js
+++ projects/tic-tak-toe-game/server-side.js
@@ -3,30 +3,30 @@ var util    = require( 'util' ),
     fs      = require( 'fs' ),
     url     = require( 'url' );
 
-function handleDefaultRequest( request, response ) {
-    fs.readFile( './webpage.html', function ( err, data ) {
-        if ( err ) {
-            response.writeHead( 404, { 'Content-type': 'text/plan' } );
-            response.write( 'Page Was Not Found' );
-            response.end();
-
-        } else {
-            response.writeHead( 200 );
-            response.write( data );
-
-            response.end();
-        }
-
-    } );
-}
+var ROOT_DIR = 'html';
 
 function onRequest( request, response ) {
-            handleDefaultRequest( request, response );
+            //handleDefaultRequest( request, response );
+    var urlObj = url.parse(request.url, true, false);
+
+    var filePath = ROOT_DIR + urlObj.pathname;
+
+    if(urlObj.pathname === '/') filePath = ROOT_DIR + '/webpage.html';
+
+    fs.readFile(filePath, function(err, data) {
+        if(err) {
+            //report error to console
+            console.log('ERROR: ' + JSON.stringify(err));
+            //respond with not found 404 to client
+            response.writeHead(404);
+            response.end();
+            return;
+        }
+        response.writeHead(200);
+        response.end(data);
+    });
 }
 
 my_http.createServer( onRequest )
     .listen( 3000 );
 console.log( "Server Running on 3000" );
-
-
-
